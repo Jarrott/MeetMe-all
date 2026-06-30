36f2d5ae8f1e050db6978b25e447e9096c182c5c
Jarrott <xqiqio7@gmail.com>
2026-07-01 02:01:46 +0630

    Add draggable conversation sidebar divider

--- TangSengDaoDaoWeb/packages/tsdaodaobase/src/Components/WKLayout/index.tsx
+++ TangSengDaoDaoWeb/packages/tsdaodaobase/src/Components/WKLayout/index.tsx
@@ -5,6 +5,9 @@ import WKViewQueue, { WKViewQueueContext } from "../WKViewQueue";
 import "./index.css"
 
 const smallScreenWidth = 640 // 小屏最大宽度（index.css @media screen 里也需要改成这个值的大小）
+const layoutLeftWidthStorageKey = "meetme_layout_left_width"
+const layoutLeftMinWidth = 260
+const layoutLeftMaxWidth = 520
 
 export enum ScreenSize {
     normal,
@@ -21,13 +24,46 @@ export interface WKLayoutProps {
 
 }
 
-export class WKLayout extends Component<WKLayoutProps>{
+export interface WKLayoutState {
+    leftWidth?: number
+    resizing: boolean
+}
+
+export class WKLayout extends Component<WKLayoutProps, WKLayoutState>{
     gResize!: (this: Window, ev: UIEvent) => any
     rightContext!: WKViewQueueContext
     routeLister!:VoidFunction
+    layoutContentRef = React.createRef<HTMLDivElement>()
+    handleLayoutResizeMouseMove = (event: MouseEvent) => {
+        const contentRect = this.layoutContentRef.current?.getBoundingClientRect()
+        if (!contentRect) {
+            return
+        }
+        const maxWidth = Math.max(layoutLeftMinWidth, Math.min(layoutLeftMaxWidth, contentRect.width * 0.48))
+        const nextWidth = Math.min(maxWidth, Math.max(layoutLeftMinWidth, event.clientX - contentRect.left))
+        this.setState({
+            leftWidth: nextWidth,
+        })
+    }
+    handleLayoutResizeMouseUp = () => {
+        window.removeEventListener("mousemove", this.handleLayoutResizeMouseMove)
+        window.removeEventListener("mouseup", this.handleLayoutResizeMouseUp)
+        document.body.classList.remove("wk-layout-resizing")
+        if (this.state.leftWidth) {
+            window.localStorage.setItem(layoutLeftWidthStorageKey, `${this.state.leftWidth}`)
+        }
+        this.setState({
+            resizing: false,
+        })
+    }
     constructor(props: any) {
         super(props)
         this.gResize = this.resize.bind(this)
+        const savedWidth = Number(window.localStorage.getItem(layoutLeftWidthStorageKey))
+        this.state = {
+            leftWidth: savedWidth >= layoutLeftMinWidth ? Math.min(savedWidth, layoutLeftMaxWidth) : undefined,
+            resizing: false,
+        }
     }
 
     componentDidMount() {
@@ -41,6 +77,9 @@ export class WKLayout extends Component<WKLayoutProps>{
 
     componentWillUnmount() {
         window.removeEventListener("resize", this.gResize)
+        window.removeEventListener("mousemove", this.handleLayoutResizeMouseMove)
+        window.removeEventListener("mouseup", this.handleLayoutResizeMouseUp)
+        document.body.classList.remove("wk-layout-resizing")
         this.rightContext.removeRouteListener(this.routeLister)
     }
 
@@ -48,16 +87,34 @@ export class WKLayout extends Component<WKLayoutProps>{
         this.setState({})
     }
 
+    handleLayoutResizeMouseDown = (event: React.MouseEvent<HTMLDivElement>) => {
+        if (window.innerWidth <= smallScreenWidth || this.props.contentFull) {
+            return
+        }
+        event.preventDefault()
+        event.stopPropagation()
+        document.body.classList.add("wk-layout-resizing")
+        window.addEventListener("mousemove", this.handleLayoutResizeMouseMove)
+        window.addEventListener("mouseup", this.handleLayoutResizeMouseUp)
+        this.setState({
+            resizing: true,
+        })
+    }
+
     render() {
         const { onRenderTab, contentLeft,contentRight,contentFull,onLeftContext,onRightContext } = this.props
+        const leftStyle = this.state.leftWidth && !contentFull && window.innerWidth > smallScreenWidth ? {
+            width: `${this.state.leftWidth}px`,
+            flexBasis: `${this.state.leftWidth}px`,
+        } : undefined
         return <div className="wk-layout">
             <div className="wk-layout-tab">
                 {
                     onRenderTab && onRenderTab(window.innerWidth <= smallScreenWidth ? ScreenSize.small : ScreenSize.normal)
                 }
             </div>
-            <div className={classNames("wk-layout-content",this.rightContext?.viewCount()>0?"wk-layout-open":undefined, contentFull ? "wk-layout-content-full-left" : undefined)}>
-                <div className="wk-layout-content-left">
+            <div ref={this.layoutContentRef} className={classNames("wk-layout-content",this.rightContext?.viewCount()>0?"wk-layout-open":undefined, contentFull ? "wk-layout-content-full-left" : undefined, this.state.resizing ? "wk-layout-content-resizing" : undefined)}>
+                <div className="wk-layout-content-left" style={leftStyle}>
                     <WKViewQueue onContext={(context) => {
                         if(onLeftContext) {
                             onLeftContext(context)
@@ -65,6 +122,7 @@ export class WKLayout extends Component<WKLayoutProps>{
                     }}>
                         {contentLeft}
                     </WKViewQueue>
+                    <div className="wk-layout-content-resizer" onMouseDown={this.handleLayoutResizeMouseDown}></div>
                 </div>
                 <div className="wk-layout-content-right">
                     <WKViewQueue onContext={(context) => {
